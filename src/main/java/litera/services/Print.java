224c16c177e8b5129d2dbda7db4865567aacdd5e
MartinTodorov00 <mzeroaha@gmail.com>
2021-12-15 10:25:01 +0200

    Fix problems with talent funnel and other small things

--- src/main/java/litera/services/Print.java
+++ src/main/java/litera/services/Print.java
@@ -4,39 +4,39 @@ import java.util.List;
 
 public class Print {
     public void print(List<ReportModel> reportModels) {
-        System.out.print("Candidates, ");
+        System.out.print("|Candidates ");
         if (reportModels.get(0).getAcceptedCandidates() != null) {
-            System.out.print("Accepted ");
+            System.out.print("|Accepted ");
         }
         if (reportModels.get(0).getAcceptedCandidatesPercent() != null) {
-            System.out.print("Percent Accepted ");
+            System.out.print("|Percent Accepted ");
         }
         if (reportModels.get(0).getSource() != null) {
-            System.out.print("Source ");
+            System.out.print("|Source ");
         }
         if (reportModels.get(0).getInterviewResult() != null) {
-            System.out.print("Result ");
+            System.out.print("|Result ");
         }
         if (reportModels.get(0).getAggregation() != null) {
-            System.out.print("Aggregation ");
+            System.out.print("|Aggregation ");
         }
         for (ReportModel reportModel : reportModels) {
             System.out.println();
-            System.out.print(reportModel.getAppliedCandidates() + ", ");
+            System.out.print("|" + reportModel.getAppliedCandidates() + "         ");
             if (reportModel.getAcceptedCandidates() != null) {
-                System.out.print(reportModel.getAcceptedCandidates() + " ");
+                System.out.print("|" + reportModel.getAcceptedCandidates() + "        ");
             }
             if (reportModel.getAcceptedCandidatesPercent() != null) {
-                System.out.print(reportModel.getAcceptedCandidatesPercent() + " ");
+                System.out.print("|" + reportModel.getAcceptedCandidatesPercent() + "               ");
             }
             if (reportModel.getSource() != null) {
-                System.out.print(reportModel.getSource() + " ");
+                System.out.print("|" + reportModel.getSource() + " ");
             }
             if (reportModel.getInterviewResult() != null) {
-                System.out.print(reportModel.getInterviewResult() + " ");
+                System.out.print("|" + reportModel.getInterviewResult() + " ");
             }
             if (reportModel.getAggregation() != null) {
-                System.out.print(reportModel.getAggregation() + " ");
+                System.out.print("|" + reportModel.getAggregation() + "");
             }
         }
     }
